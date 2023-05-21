9a48917453fc4ffe64ad89e90df0548bda9c01c8
Alexandr Zhdanko <zhdko@yandex.ru>
2023-05-21 21:34:30 +0400

    fix: Изменения опций куков

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -51,9 +51,7 @@ const login = (req, res, next) => {
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
-          sameSite: 'none',
-          secure: true,
-          httpOnly: true,
+          sameSite: true,
         })
         .send({ token });
     })
